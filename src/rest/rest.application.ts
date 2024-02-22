81a7f89296b8ad38dbfd71d726543d97ab3ed5a4
Sergey Kotelnikov <newelvin@gmail.com>
2024-02-22 23:12:41 +0300

    feat: создание и запуск сервера express

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,19 +1,22 @@
 import { inject, injectable } from 'inversify';
+import express, { Express } from 'express';
 import { Config, RestSchema } from '../shared/libs/config/index.js';
 import { Logger } from '../shared/libs/logger/index.js';
 import { Component } from '../shared/types/component.enum.js';
 import { DatabaseClient } from '../shared/libs/database-client/database-client.interface.js';
 import { getMongoURI } from '../shared/helpers/database.js';
-import { OfferService } from '../shared/modules/offer/offer-service.interface.js';
 
 @injectable()
 export class RestApplication {
+  private readonly server: Express;
+
   constructor(
     @inject(Component.Logger) private readonly logger: Logger,
     @inject(Component.Config) private readonly config: Config<RestSchema>,
-    @inject(Component.DatabaseClient) private readonly databaseClient: DatabaseClient,
-    @inject(Component.OfferService) private readonly offerService: OfferService
-  ) {}
+    @inject(Component.DatabaseClient) private readonly databaseClient: DatabaseClient
+  ) {
+    this.server = express();
+  }
 
   private async _initDB() {
     const mongoUri = getMongoURI(
@@ -27,16 +30,20 @@ export class RestApplication {
     return this.databaseClient.connect(mongoUri);
   }
 
+  private async _initServer() {
+    const port = this.config.get('PORT');
+    this.server.listen(port);
+  }
+
   public async init() {
     this.logger.info('Application initialization');
-    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
-    this.logger.info(`Get value from env $DB_HOST: ${this.config.get('DB_HOST')}`);
 
     this.logger.info('Init database...');
     await this._initDB();
     this.logger.info('Init database completed.');
 
-    const res = await this.offerService.findAll();
-    console.log('RES: ', res);
+    this.logger.info('Try to init server...');
+    await this._initServer();
+    this.logger.info(`🚀 Server started on http://localhost:${this.config.get('PORT')}`);
   }
 }
