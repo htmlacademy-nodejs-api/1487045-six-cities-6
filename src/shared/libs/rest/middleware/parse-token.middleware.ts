7716681c40939096387875338ee2b942df3f14b6
Sergey Kotelnikov <newelvin@gmail.com>
2024-03-06 00:46:29 +0300

    feat: исправить middleware

--- src/shared/libs/rest/middleware/parse-token.middleware.ts
+++ src/shared/libs/rest/middleware/parse-token.middleware.ts
@@ -12,10 +12,8 @@ function isTokenPayload(payload: unknown): payload is TokenPayload {
     payload !== null &&
     'email' in payload &&
     typeof payload.email === 'string' &&
-    'firstname' in payload &&
-    typeof payload.firstname === 'string' &&
-    'lastname' in payload &&
-    typeof payload.lastname === 'string' &&
+    'name' in payload &&
+    typeof payload.name === 'string' &&
     'id' in payload &&
     typeof payload.id === 'string'
   );
